9e44e822e5b3c9ea3ad0952b9359569137b8430d
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2023-09-23 09:55:37 -0300

    Initial commit

---
+++ src/main/java/com/m1guelsb/blog/M1guelsbBlogApplication.java
@@ -0,0 +1,13 @@
+package com.m1guelsb.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M1guelsbBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M1guelsbBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/m1guelsb/blog/M1guelsbBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m1guelsb.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M1guelsbBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
