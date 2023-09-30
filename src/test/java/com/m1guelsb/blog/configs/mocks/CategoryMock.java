2a02fc67f8d55e7477a4383c9b2c3569239d0545
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2023-09-30 12:16:24 -0300

    [test] category mocks - add dto mock methods

--- src/test/java/com/m1guelsb/blog/configs/mocks/CategoryMock.java
+++ src/test/java/com/m1guelsb/blog/configs/mocks/CategoryMock.java
@@ -3,12 +3,17 @@ package com.m1guelsb.blog.configs.mocks;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.m1guelsb.blog.dtos.CategoryDto;
 import com.m1guelsb.blog.entities.Category;
 
 public class CategoryMock {
 
   public Category mockEntity() {
     return mockEntity(0);
+  }
+
+  public CategoryDto mockDto() {
+    return mockDto(0);
   }
 
   public List<Category> mockEntityList() {
@@ -24,5 +29,9 @@ public class CategoryMock {
     category.setId(number.longValue());
     category.setTitle("Category" + number);
     return category;
+  }
+
+  public CategoryDto mockDto(Integer number) {
+    return new CategoryDto(number.longValue(), "Category" + number);
   }
 }
